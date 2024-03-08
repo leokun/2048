f247a15e2c8909c27f34c9119267818d38794da7
Léo Stewart <leokun@me.com>
2024-03-08 21:28:40 +0100

    add some custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,8 +6,20 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  theme: {   
     extend: {
+      colors: {
+        'custom-blue': {
+          '2': '#010012',
+          '4': '#03002f',
+          '8': '#05004d',
+          '16': '#06006b',
+          '32': '#070088',
+          '64': '#0700a6',
+          '128': '#0500c4',
+          '256': '#0300e1'
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
