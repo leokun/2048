759c173eb534985591e2213d2375c56385332130
Léo Stewart <leokun@me.com>
2024-03-14 14:52:36 +0100

    again

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import Matrice from "@/components/2048";
+import Score from "@/components/2048/Score";
 import { GameContextProvider } from "@/components/2048/context";
 
 export default function Home() {
 
   return <GameContextProvider>
+    <Score/>
     <Matrice />
   </GameContextProvider>
 }
--- components/2048/Matrice.tsx
+++ components/2048/Matrice.tsx
@@ -7,11 +7,11 @@ import { GameContext } from "./context";
 
 export default function Matrice() {
 
-  const gameState = useContext(GameContext)
+  const {matrice} = useContext(GameContext)
   return (
     <div
     className="grid grid-cols-4 gap-4 p-4 h-80 w-80 bg-slate-900">
-      {gameState.matrice.flat().map((cell, index) => (
+      {matrice.flat().map((cell, index) => (
         <Tile key={index} value={cell} />
       ))}
     </div>
