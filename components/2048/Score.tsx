fa57e42611b09eb15d05c348319f17872d51affc
Léo Stewart <leokun@me.com>
2024-03-14 14:52:18 +0100

    add total sub component

---
+++ components/2048/Score.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { GameContext } from "./context"
+
+export default function Score() {
+
+    const { score } = useContext(GameContext)
+
+    return <div className="p4 h-12 bg-slate-900 flex items-center justify-center text-3xl box-content">
+        Score: {score}
+    </div>
+}
