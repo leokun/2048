cc303171caa4677bd0a356fbe333af0592f07616
Léo Stewart <leokun@me.com>
2024-03-17 22:27:01 +0100

    Enhance display Fixes #4

--- components/2048/Score.tsx
+++ components/2048/Score.tsx
@@ -5,7 +5,7 @@ export default function Score() {
 
     const {state: {score}} = useContext(GameContext)
 
-    return <div className="p4 h-12 bg-slate-900 flex items-center justify-center text-3xl box-content">
+    return <div className="bg-slate-900 flex items-center justify-center box-content">
         Score: {score}
     </div>
 }
