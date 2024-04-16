6fad0cf1a114c3276618c42b96befb98988c382d
Léo Stewart <leokun@me.com>
2024-04-16 23:12:04 +0200

    disable moves

--- components/2048/Matrice.tsx
+++ components/2048/Matrice.tsx
@@ -13,7 +13,7 @@ export default function Matrice() {
   } = useContext(GameContext);
 
   return (
-    <div className="relative h-80 w-80 bg-slate-900 mt-2">
+    <div className="relative h-80 w-80 bg-slate-900 mt-2 touch-none">
       <div className=" absolute grid grid-cols-4 gap-4 p-4 h-80 w-80">
         {matrice.flat().map((cell) => (
           <Tile key={uuid()} value={cell} />
